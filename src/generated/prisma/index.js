eb04288fdd20f7f7119fa2fbd4f1d9d43746a0cd
nolannmoisis <moisis@et.esiea.fr>
2025-04-20 19:19:47 +0200

    Prisma

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -202,7 +202,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -212,11 +212,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_bt7ZDlqI2vnf@ep-hidden-dust-a2uf8ivr-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
